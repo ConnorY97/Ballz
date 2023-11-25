f60a61d8706c11eb38bdec3f636fe64d3221067c
Connor Young <c_young97@live.com>
2023-11-25 16:29:13 +1100

    Added ball spawning, input interaction and an indicator for the direction and force of the shot. Things are not colliding which is the next thing to fix

--- BallsGame/Assets/Scripts/GameManager.cs
+++ BallsGame/Assets/Scripts/GameManager.cs
@@ -6,67 +6,143 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	//Inspector variables----------------------
-	public int rows;
-	public int cols;
-	public float cellSize;
-	public float xSpacing;
-	public Camera mainCamera;
-	public Box prefab;
+    //Inspector variables----------------------
+    public int rows;
+    public int cols;
+    public float cellSize;
+    public float xSpacing;
+    public Camera mainCamera;
+    public Box boxPrefab;
 
-	private List<Box> spawnRow;
+    private List<Box> spawnRow;
 
-	//Private varaibles-----------------------
-	private int _roundCounter = 0;
-	public List<Box> boxes;
+    //Private varaibles-----------------------
+    private int _roundCounter = 0;
+    public List<Box> boxes;
 
-	// Ball stuff
-	public GameObject ballSpawn;
-	public GameObject ballSpawnPrefab;
+    // Ball stuff
+    public GameObject ballSpawn;
+    public Ball ballPrefab;
 
-	private void Start()
-	{
-		SpawnBox();
-	}
+    // Click stuff
+    private Vector3 clickPos = Vector3.zero;
+    private Vector3 dragPos = Vector3.zero;
+    private Vector3 shootDir = Vector3.zero;
+    private float shootForce = 0.0f;
+    public LineRenderer drag;
+    public float multiplyier = 100.0f;
+    private int ballz = 1;
 
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			++_roundCounter;
-			MoveBox();
-		}
-	}
+    public List<Ball> balls = new List<Ball>();
 
-	public void SpawnBox()
-	{
-		int amountToSpawn = Random.Range(1, cols + 1);
+    private bool isShooting = false;
 
-		for (int i = 0; i < amountToSpawn; i++)
-		{
-			int spawnChance = Random.Range(0, 10);
-			if (spawnChance < 6)
-			{
-				Box box = Instantiate(prefab, transform);
-				box.transform.position = new Vector2(transform.position.x + (i * (cellSize + xSpacing)), transform.position.y);
-				box.col = i;
-				box.row = 0;
-				boxes.Add(box);
-			}
-		}
-	}
+    private void Start()
+    {
+        SpawnBox();
+        SpawnBall();
+        drag.SetPosition(0, ballSpawn.transform.position);
+        drag.SetPosition(1, ballSpawn.transform.position);
+    }
 
-	public void MoveBox()
-	{
-		for (int i = 0; i < boxes.Count; i++)
-		{
-			boxes[i].transform.position = new Vector2(boxes[i].transform.position.x, boxes[i].transform.position.y - cellSize);
-			boxes[i].row += 1;
-		}
-		SpawnBox();
-	}
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ++_roundCounter;
+            MoveBox();
+        }
 
-	public void SpawnBall()
-	{
-	}
+        if (!isShooting)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                drag.SetPosition(0, ballSpawn.transform.position);
+                clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                shootDir = Vector3.zero;
+                shootForce = 0.0f;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                dragPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 endPoint = ballSpawn.transform.position + (clickPos - dragPos);
+                endPoint.z = 0;
+                drag.SetPosition(1, endPoint);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                shootForce = Vector3.Distance(clickPos, dragPos);
+                shootDir = (drag.GetPosition(1) - ballSpawn.transform.position).normalized;
+                Shoot(shootDir, shootForce);
+                drag.SetPosition(1, ballSpawn.transform.position);
+                clickPos = Vector3.zero;
+                dragPos = Vector3.zero;
+            }
+        }
+    }
+
+    public void SpawnBox()
+    {
+        int amountToSpawn = Random.Range(1, cols + 1);
+
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            int spawnChance = Random.Range(0, 10);
+            if (spawnChance < 6)
+            {
+                Box box = Instantiate(boxPrefab, transform);
+                box.transform.position = new Vector2(transform.position.x + (i * (cellSize + xSpacing)), transform.position.y);
+                box.col = i;
+                box.row = 0;
+                boxes.Add(box);
+            }
+        }
+    }
+
+    public void MoveBox()
+    {
+        for (int i = 0; i < boxes.Count; i++)
+        {
+            boxes[i].transform.position = new Vector2(boxes[i].transform.position.x, boxes[i].transform.position.y - cellSize);
+            boxes[i].row += 1;
+        }
+        SpawnBox();
+    }
+
+    public void SpawnBall()
+    {
+        for(int i = 0;i < ballz ;i++)
+        {
+            Ball ball = Instantiate(ballPrefab, transform);
+            ball.transform.position = ballSpawn.transform.position;
+            Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            balls.Add(ball);
+        }
+    }
+
+    private void Shoot(Vector3 dir, float force)
+    {
+        Debug.Log("Dir: " + dir + " force: " + force);
+        isShooting = true;
+        if (dir != Vector3.zero || force != 0f)
+        {
+            for (int i = 0; i < balls.Count; i++)
+            {
+                Rigidbody2D rb = balls[i].GetComponent<Rigidbody2D>();
+                if (rb != null)
+                {
+                    rb.gravityScale = 1;
+                    rb.AddForce((dir *  force) * multiplyier);
+                }
+                else
+                {
+                    Debug.Log("RB null");
+                }
+            }
+        }
+        isShooting = false;
+    }
 }
