6540051368d840d1b1df6958ed58aa9e3395d8bc
Connor Young <c_young97@live.com>
2023-12-15 14:19:32 +1100

    Update DoNotDestroy.cs

--- Ballz/Assets/Scripts/DoNotDestroy.cs
+++ Ballz/Assets/Scripts/DoNotDestroy.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class DoNotDestroy : MonoBehaviour
 {
+    private static DoNotDestroy instance;
     private void Awake()
     {
         DontDestroyOnLoad(this);
+
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
     }
 }
